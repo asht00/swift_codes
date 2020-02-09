e434c4e4c450d8cc7b46ef88d29027b900ef3c9f
Ashima T <ashimat@MacBook-Pro.local>
2020-02-09 11:50:51 +0530

    added task 4 files

---
+++ ADG_task4/ADG_task4/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  ADG_task4
+//
+//  Created by Ashima T on 09/02/20.
+//  Copyright © 2020 Ashima T. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        func first20even(){
+            var i = 0
+            while(i <= 40){
+                print(i)
+                i += 2
+            }
+        }
+        
+        func longstring(str1: String, str2: String){
+            if( str1.count > str2.count)
+            {
+                print(str1)
+            }
+            else
+            {
+                print(str2)
+            }
+        }
+        
+        func strswap( key: Int ){
+            var arr = ["abc", "def", "ghi", "jkl", "mno"]
+            arr.swapAt(0, key)
+            
+        }
+        
+        //function calls
+        print(first20even())
+        print(longstring(str1: "adg", str2: "task"))
+        print(strswap(key: 3))
+    }
+
+
+}
+
---
+++ ADG_task4/ADG_task4Tests/ADG_task4Tests.swift
@@ -0,0 +1,34 @@
+//
+//  ADG_task4Tests.swift
+//  ADG_task4Tests
+//
+//  Created by Ashima T on 09/02/20.
+//  Copyright © 2020 Ashima T. All rights reserved.
+//
+
+import XCTest
+@testable import ADG_task4
+
+class ADG_task4Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
